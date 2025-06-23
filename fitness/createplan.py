2a1f40a5b052e2bbded0ceb032231909a75c6f4d
Phat <phat.chauu26@gmail.com>
2025-06-23 16:25:16 +0700

    base completed

--- fitness/createplan.py
+++ fitness/createplan.py
@@ -4,6 +4,6 @@ from fitness.database import Plan
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.menu import MenuLink
 
-create_plan = Admin (app=app, name="FitPal", template_mode='bootstrap4', url='/admin')
+create_plan = Admin (app=app, name="ProFitness", template_mode='bootstrap4', url='/admin')
 create_plan.add_view(ModelView(Plan, db.session))
 create_plan.add_link(MenuLink(name='Back to Main Page', url='/home'))
